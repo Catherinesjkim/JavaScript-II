2c8dab8b2ec616d1befdb452d4fc0bb58832044d
Catherine Kim <cat@Catherines-MacBook-Air.local>
2019-10-04 12:55:45 -0700

    Add Tast 1: Higher Order Functions and Calbacks Because: - This task focuses on getting practice with higher order functions and callback functions by giving us an array of values and instructions on what to do with that array
    This commit:
    -Completed the problems provide to me but skipped over stretch problems until I'm complete with every other JS file first.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,9 +1,7 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
-
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array. Study both the problem and the solution to figure out the rest of the problems.
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// firstItem passes the first item of the given array to the callback function.
 /* 
-
   // GIVEN THIS PROBLEM:
 
   function firstItem(arr, cb) {
@@ -33,33 +31,71 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   function logExorbitantPrice(article) {
     return `this ${article} is worth a million dollars!`;
   };
-
   const test2 = firstItem(items, logExorbitantPrice);
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// firstItem passes the first item of the given array to the callback function.
 
+function firstItem(arr, cb) {
+  return cb(arr[0]);
+};
+// TEST 1 (inlined callback):
+const test1 = firstItem(items, item => `I love my ${item}!`);
+// console.log(test1);
+
+function logExorbitantPrice(article) {
+  return `this ${article} is worth a million dollars!`;
+};
+// TEST 2 (declaring callback before hand):
+const test2 = firstItem(items, logExorbitantPrice);
+// console.log(test2);
+
+// TEST 3 
+function lastItem(arr, cb) {
+  // lastItem passes the last item of the array into the callback.
+  return cb(arr[3]);
+};
+const test3 = lastItem(items, item => `I love my ${item}!`);
+// console.log(test3);
+
+// TEST 4
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
-
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
+const test4 = getLength(items, arrLength => `This list is ${arrLength} items long!`);
+// console.log(test4);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+sumNums(2, 4, function(x, y) {
+  // console.log(x + y);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+multiplyNums(2, 4, function(x, y) {
+  // console.log(x * y)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item)){
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
-
+contains('Pencil', items, function(result){
+  console.log(result);
+})
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
